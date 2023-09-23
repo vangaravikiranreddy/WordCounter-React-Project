a9302c6ecfbebe6ae1f17fe74f24350b98f31642
vangaravikiranreddy <92626377+vangaravikiranreddy@users.noreply.github.com>
2023-09-23 20:34:47 +0530

    Features of word Counter
    --->Added methods like onChange() which process the existing text and gives you the count in the TextForm Component.
    -->Features such as converting to lower and upper case, and copying the text with a button click.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+//import AboutUs from './components/AboutUs';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
 
 function App() {
+  const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+     setAlert({
+      msg: message,
+      type: type
+     })
+     setTimeout(() =>{
+      setAlert(null);
+     }, 3000);
+  }
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+      showAlert("dark mode has been enabled", "success");
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+      showAlert("light mode has been enabled", "success");
+    }
+  } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+       {/* <Navbar title="TextUtils" aboutText="About Us"/> */}
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+        <TextForm showAlert={showAlert} heading="Enter the text to anaylyze below" alert={alert} mode={mode} />
+       {/* <AboutUs /> */}
+        </div>
+      </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react'
+
+
+export default function TextForm(props) {
+  const handleUpClick = ()=>{
+   // console.log("Upper Case was clicked" + text);
+    let newText = text.toUpperCase();
+    setText(newText)
+    props.showAlert("Enabled to upper case", "success");
+  }
+
+  const handleLoClick = ()=>{
+  //  console.log("Lower Case was clicked" + text);
+    let newText = text.toLowerCase();
+    setText(newText)
+    props.showAlert("Enabled to lower case", "success");
+  }
+
+  const handleCopyText = ()=>{
+       var text = document.getElementById("myBox");
+       text.select();
+       navigator.clipboard.writeText(text.value);
+       props.showAlert("Enabled to copy text", "success");
+  }
+
+  const handleOnChange = (event)=>{
+   // console.log("On change");
+    setText(event.target.value);
+  }
+
+
+
+  const [text, setText] = useState('Enter text here');
+  //text = "new text"; // wrong way to change state
+  //setText("new Text"); // Correct way to change text
+  return (
+    <>
+    <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
+        <h1>{props.heading}</h1>
+     <div className="mb-3">
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="3"></textarea>
+     </div>
+     <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to upper Case</button>
+     <button className='btn btn-primary mx-2' onClick={handleLoClick}>Convert to Lower Case</button>
+     <button className='btn btn-primary mx-2' onClick={handleCopyText}>Copy Text</button>
+    </div>
+    <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
+         <h2>Your text Summary</h2>
+         <p>{text.split(" ").length} words and {text.length} characters</p>
+         <p>{0.008 * text.split(" ").length} Minutes read</p>
+         <h2>Preview</h2>
+         <p>{text}</p>
+    </div>
+    </>
+  )
+}
